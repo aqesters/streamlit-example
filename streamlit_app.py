edeb58ea1872aafb7128f14d2331de0a603f8e2f
Ari E <133052948+aqesters@users.noreply.github.com>
2023-09-25 18:18:13 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,23 @@
 from collections import namedtuple
 import altair as alt
-import math
 import pandas as pd
 import streamlit as st
 
 """
-# Welcome to Streamlit!
+# AnalyzeTrades v1.3
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
+Author: Ari Esters
+Last Modified: 25 Sept 2023
 """
 
-
 with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 10000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+    n_days = st.slider("Analyze data over last __ days:", 1, 365, 30)
 
     Point = namedtuple('Point', 'x y')
     data = []
 
     points_per_turn = total_points / num_turns
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+    #st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
+     #   .mark_circle(color='#0068c9', opacity=0.5)
+      #  .encode(x='x:Q', y='y:Q'))
